d3e432fe808d553f8a4da4667d8b5edc4e49ba30
shubham <shubhamsinghchauhan890@gmail.com>
2025-06-26 10:12:37 +0530

    Initial commit - Terraform Lambda

---
+++ main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Effect = "Allow",
+      Sid    = ""
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "node_app" {
+  function_name = "node-app"
+  role          = aws_iam_role.lambda_exec.arn
+  handler       = "app.handler"
+  runtime       = "nodejs18.x"
+  filename      = "lambda.zip"
+  source_code_hash = filebase64sha256("lambda.zip")
+
+  environment {
+    variables = {
+      NODE_ENV = "production"
+    }
+  }
+}
