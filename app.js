a429c8e46e4a974d41feba2f33014f2a3911e77e
SHUBHAM KUMAR <143086699+SHUBHAMKUMAR900@users.noreply.github.com>
2025-07-01 10:45:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello from Lambda on Windows!" }),
+    body: JSON.stringify({ message: "Hello from cloud techner" }),
   };
 };
