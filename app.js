11c22c770b44579283c9d84fc6e3fb1c3ea5a3e2
SHUBHAM KUMAR <143086699+SHUBHAMKUMAR900@users.noreply.github.com>
2025-07-02 13:17:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello from cloud techner one" }),
+    body: JSON.stringify({ message: "Hello from cloud techner one two " }),
   };
 };
